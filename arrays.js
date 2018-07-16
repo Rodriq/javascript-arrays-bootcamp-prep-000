a872db756449c885b3028e8ba626ba37a5b9e494
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-16 13:07:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,7 @@ function accessElementInArray(array, index){
 			array.shift();
 			return array;
 	}
+	function removeElementFromBeginningOfArray(array){
+			array.slice(0);
+			return array;
+		}
